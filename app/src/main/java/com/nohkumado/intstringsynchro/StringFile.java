e602a2d709302d1beec02eec50fdd6c59d69b337
NohKumaDo <nohkumado@gmail.com>
2017-01-25 22:34:56 +0100

    added javadoc

--- app/src/main/java/com/nohkumado/intstringsynchro/StringFile.java
+++ app/src/main/java/com/nohkumado/intstringsynchro/StringFile.java
@@ -6,57 +6,55 @@ import java.net.*;
  * @licence GLP v3
  * @version  "%I%, %G%",
  * 
+ * a file subclass that holds the additional info about which langugage it represents 
  */
-
 public class StringFile extends File
 {
+  /** the lang for which we store this file */
   String lang;
 
   /**
-   Creates a new File instance from a parent abstract pathname and a child pathname string.
+   *  Creates a new File instance from a parent abstract pathname and a child pathname string.
    */
   public StringFile(File parent, String child, String la)
   {
     super(parent, child);
     lang = la;
-  }
+  }//CTOR
   /** 
-   Creates a new File instance by converting the given pathname string into an abstract pathname.
-
+   * Creates a new File instance by converting the given pathname string into an abstract pathname.
    */
   public StringFile(String pathname, String la)
   {
     super(pathname);
     lang = la;
-  }/**
-   Creates a new File instance from a parent pathname string and a child pathname string.
-
+  }//CTOR
+  /**
+   * Creates a new File instance from a parent pathname string and a child pathname string.
    */
   public StringFile(String parent, String child, String la)
   {
     super(parent, child);
     lang = la;
-  }/**
-   Creates a new File instance by converting the given file: URI into an abstract pathname.
-
+  }//CTOR
+  /**
+   * Creates a new File instance by converting the given file: URI into an abstract pathname.
    */
   public StringFile(URI uri, String la)
   {
     super(uri);
     lang = la;
-  }
-
-/**
-* setter getter for the language
-*/
+  }//CTOR
+  /**
+   * setter getter for the language
+   */
   public void lang(String lang)
   {
     this.lang = lang;
-  }
-
+  }//public void lang(String lang)
+  /** getter */
   public String lang()
   {
     return lang;
-  }
-
-}//class
+  }//public String lang()
+}//public class StringFile extends File
