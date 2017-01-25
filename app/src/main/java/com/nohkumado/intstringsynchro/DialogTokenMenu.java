94b6e0be0179e890bc524e6e61c1df856477787a
NohKumaDo <nohkumado@gmail.com>
2017-01-25 21:11:16 +0100

    added javadoc

--- app/src/main/java/com/nohkumado/intstringsynchro/DialogTokenMenu.java
+++ app/src/main/java/com/nohkumado/intstringsynchro/DialogTokenMenu.java
@@ -9,43 +9,52 @@ import android.widget.*;
  * @licence GLP v3
  * @version  "%I%, %G%",
  * 
-*/
+ * floating menu over token
+ */
 public class DialogTokenMenu extends PopupMenu implements PopupMenu.OnMenuItemClickListener
 {
-
+  /** the interface to be able to pass back stuff to the callback
+   */
   public interface TokenDialogListener
   {
+    /** the method the callback shoud implement */
     void onFinishTokenDialog(String token, String action);
-  }
+  }//public interface TokenDialogListener
 
-  public static final String TAG = "DiaTok";
-  TokenDialogListener listener;
-  Button delete, copy, rename,cancel;
+  public static final String TAG = "DiaTok"; /** needed for Log.d */
+  TokenDialogListener listener; /** the callback */
   String token = "";
-
+  /**
+   * CTOR
+   */
   public DialogTokenMenu(Context context, View anchor, String token)
   {
-    super(context,anchor);
+    super(context, anchor);
     this.token = token;
-  }
-
+  }//public DialogTokenMenu(Context context, View anchor, String token)
+  /**
+   * CTOR
+   */
   public DialogTokenMenu(Context context, View anchor, int gravity, String token)
   {
-    super(context,anchor,gravity);
+    super(context, anchor, gravity);
     this.token = token;
-  }
-
-  public DialogTokenMenu(Context context, View anchor, int gravity, int popupStyleAttr, int popupStyleRes,String token) 
+  }//public DialogTokenMenu(Context context, View anchor, int gravity, String token)
+  /**
+   * CTOR
+   */
+  public DialogTokenMenu(Context context, View anchor, int gravity, int popupStyleAttr, int popupStyleRes, String token) 
   {
-    super(context,anchor,gravity,popupStyleAttr,popupStyleRes);
+    super(context, anchor, gravity, popupStyleAttr, popupStyleRes);
     this.token = token;
-  }
-
-
+  }//public DialogTokenMenu(Context context, View anchor, int gravity, int popupStyleAttr, int popupStyleRes, String token) 
+  /**
+   * catch click on menu item
+   */
   @Override
   public boolean onMenuItemClick(MenuItem p1)
   {
-    switch(p1.getItemId())
+    switch (p1.getItemId())
     {
       case R.id.token_copy:
         listener.onFinishTokenDialog(token, "copy");
@@ -56,15 +65,14 @@ public class DialogTokenMenu extends PopupMenu implements PopupMenu.OnMenuItemCl
       case R.id.token_rename:
         listener.onFinishTokenDialog(token, "rename");
         break;
-    }
-    
+    }//switch (p1.getItemId())
     return true;
-  }
-  
+  }//public boolean onMenuItemClick(MenuItem p1)
+  /**
+   * set the callback
+   */
   public void setTokenDialogListener(TokenDialogListener p0)
   {
     listener = p0;
-  }
-  
-  
-}
+  }//public void setTokenDialogListener(TokenDialogListener p0)
+}//public class DialogTokenMenu extends PopupMenu implements PopupMenu.OnMenuItemClickListener
